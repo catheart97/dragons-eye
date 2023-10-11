6e8c8d320e37c44c613290cb49d1886ed4f6ebf9
Ronja Schnur <ronjaschnur@uni-mainz.de>
2023-10-11 18:19:07 +0200

    :sparkles: Layout Fix.

--- source/board/BoardComponent.tsx
+++ source/board/BoardComponent.tsx
@@ -245,6 +245,7 @@ const drawBoard = (canvas: HTMLCanvasElement, board: Board, playerView: boolean)
             }
         }
     }
+    
 
     // draw scene
     forEachTile(drawTerrain);
@@ -341,7 +342,7 @@ const BoardComponentRenderer: React.ForwardRefRenderFunction<BoardComponentHandl
             }}>
                 <div className="p-72 flex flex-col">
                     <div className="sticky bg-black/80 text-white text-2xl w-full self-end" style={{
-                        zIndex: 1000,
+                        zIndex: 50,
                         height: CanvasBaseSize + 'px',
                         width: props.board.width * CanvasBaseSize + 'px',
                         top: 0,
@@ -372,7 +373,7 @@ const BoardComponentRenderer: React.ForwardRefRenderFunction<BoardComponentHandl
                     </div>
                     <div className="flex">
                         <div className="sticky bg-black/80 text-white text-2xl" style={{
-                            zIndex: 1000,
+                            zIndex: 50,
                             height: props.board.height * CanvasBaseSize + 'px',
                             width: CanvasBaseSize + 'px',
                             left: 0,
